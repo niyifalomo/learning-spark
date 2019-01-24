c8e214679c00aa9b05de8eee6a731526791e1207
Olaniyi Falomo <niyifalomo@gmail.com>
2019-01-24 12:02:25 -0500

    Refactored LineCount

--- src/main/java/com/olaniyi/learningSpark/LineCount.java
+++ src/main/java/com/olaniyi/learningSpark/LineCount.java
@@ -7,17 +7,17 @@ import org.apache.spark.sql.SparkSession;
  * Find the number of errors and info logs in log file
  * i.e number of lines starting with 'ERROR' and 'INFO'
  */
-public class SimpleFilterExample {
+public class LineCount {
     public static void main(String[] args){
 
         String logFile = "input/log.txt";
-        SparkSession spark = SparkSession.builder().appName("Simple Application").master("local[1]").getOrCreate();
+        SparkSession spark = SparkSession.builder().appName("Line Counter").master("local[1]").getOrCreate();
         Dataset<String>  logData = spark.read().textFile(logFile).cache();
 
         long numErrors = logData.filter(line -> line.startsWith("ERROR")).count();
         long numInfos = logData.filter(line -> line.startsWith("INFO")).count();
 
-        System.out.format("-Lines Starting with ERROR: %d%n-Lines with INFO: %d%n", numErrors,numInfos);
+        System.out.format("-Number of lines starting with ERROR: %d%n-Number of lines starting with INFO: %d%n", numErrors,numInfos);
         spark.stop();
 
 
